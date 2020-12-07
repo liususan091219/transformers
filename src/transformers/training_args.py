ea3642b8ccc1f7dc1cb6218e48921945a327ae63
Xueqing Liu <xliu127@tmdev.cs.stevens.edu>
2020-12-06 20:10:33 -0800

    backup before adding superglue

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -182,6 +182,8 @@ class TrainingArguments:
             - :obj:`False` if :obj:`metric_for_best_model` is not set, or set to :obj:`"loss"` or :obj:`"eval_loss"`.
     """
 
+    """ the following parameters are defined by myself """
+
     output_dir: str = field(
         metadata={"help": "The output directory where the model predictions and checkpoints will be written."}
     )
@@ -194,7 +196,6 @@ class TrainingArguments:
             )
         },
     )
-
     do_train: bool = field(default=False, metadata={"help": "Whether to run training."})
     do_eval: bool = field(default=None, metadata={"help": "Whether to run eval on the dev set."})
     do_predict: bool = field(default=False, metadata={"help": "Whether to run predictions on the test set."})
@@ -254,6 +255,7 @@ class TrainingArguments:
         default=-1,
         metadata={"help": "If > 0: set total number of training steps to perform. Override num_train_epochs."},
     )
+    warmup_ratio: float = field(default=0.0, metadata={"help": "Linear warmup ratio"})
     warmup_steps: int = field(default=0, metadata={"help": "Linear warmup over warmup_steps."})
 
     logging_dir: Optional[str] = field(default_factory=default_logdir, metadata={"help": "Tensorboard log dir."})
@@ -453,7 +455,7 @@ class TrainingArguments:
         """
         Serializes this instance to a JSON string.
         """
-        return json.dumps(self.to_dict(), indent=2)
+        return json.dumps(str(self.to_dict()), indent=2)
 
     def to_sanitized_dict(self) -> Dict[str, Any]:
         """
